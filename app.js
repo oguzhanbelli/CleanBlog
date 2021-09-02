f98a02b880dec60d8e1fea92f2be4b3cd10c8334
Oğuzhan Belli <oguzhanbellii@gmail.com>
2021-09-02 17:13:30 +0300

    Template Engine & Task 7

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
+app.set('view engine', 'ejs');
+const path = require('path');
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+  res.sendFile(path.resolve(__dirname,'views/index.html'));
+  res.render("index");
+});
 
-  res.send(blog);
+app.get('/about', function (req, res) {
+  res.sendFile(path.resolve(__dirname,'views/about.html'));
+  res.render("about");
+});
+  
+app.get('/add', function (req, res) {
+  res.sendFile(path.resolve(__dirname,'views/add_post.html'));
+  res.render("add_post");
 });
 const port = 3000;
 app.listen(port, () => {
