847d5186ce6d6a41514375bbc07ee2f60817a743
Oğuzhan Belli <oguzhanbellii@gmail.com>
2021-09-03 21:07:03 +0300

    Task 8 & Add Post & List Posts

--- app.js
+++ app.js
@@ -1,27 +1,50 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const moment = require("moment")
+
 const ejs = require('ejs');
+const path = require('path');
+const Post = require('./models/Post');
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//DB CONNECT
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 //Template Engine
 app.set('view engine', 'ejs');
-const path = require('path');
+
 //MIDDLEWARES
 app.use(express.static('public'));
 
-
 //ROUTES
-app.get('/', function (req, res) {
+app.get('/', async(req, res) => {
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  const posts = await Post.find({});
 
-  res.render("index");
+
+  res.render('index', {
+    posts,
+    moment:moment
+  });
 });
 
 app.get('/about', function (req, res) {
-  
-  res.render("about");
+  res.render('about');
 });
 
 app.get('/add', function (req, res) {
-  
-  res.render("add_post");
+  res.render('add_post');
+});
+
+app.post('/add_post', function (req, res) {
+  Post.create(req.body);
+
+  res.redirect("/");
 });
 const port = 3000;
 app.listen(port, () => {
---
+++ models/Post.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//create Schema
+
+const PostSchema = new Schema({
+  title: String,
+  detail: String,
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Post = mongoose.model('Post', PostSchema);
+
+
+module.exports = Post;
+
+
+
+
